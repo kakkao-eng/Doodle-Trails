6ff6d11089eb8e6fe6a3a7ef8b4ccf6da5dfbb89
Hayashi322 <theethawat.muak@bumail.net>
2024-11-28 01:16:54 +0700

    BG Sound

--- Assets/Earth/Script/Obstacle.cs
+++ Assets/Earth/Script/Obstacle.cs
@@ -1,4 +1,4 @@
-using System;
+﻿using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -6,43 +6,72 @@ using UnityEngine.SocialPlatforms.Impl;
 
 public class Obstacle : MonoBehaviour
 {
-    public Test player;
-    public Score score;
-    public GameObject endScreen;
-    public GameObject ScoreCount;
-    public GameObject UIcontorl;
-    
-    // Start is called before the first frame update
-    void Start()
-    {
-        //playerMovement = GameObject.FindObjectOfType<Test>();
+    // อ้างอิงถึงสคริปต์และ GameObject ต่างๆ
+    public Test player;                    // สคริปต์ที่ควบคุมการเคลื่อนที่ของผู้เล่น
+    public Score score;                    // สคริปต์จัดการคะแนน
+    public GameObject endScreen;           // จอแสดงผลเมื่อเกมจบ
+    public GameObject ScoreCount;          // UI แสดงคะแนนขณะเล่น
+    public GameObject UIControl;           // UI ควบคุมอื่นๆ
+    public BackgroundMusicController musicController; // ควบคุมเพลงพื้นหลัง
 
+    private void Start()
+    {
+        // หากไม่ได้ตั้งค่าตัวแปรใน Inspector อาจค้นหา GameObject อัตโนมัติ
+        if (player == null)
+        {
+            player = GameObject.FindObjectOfType<Test>();
+        }
+
+        if (score == null)
+        {
+            score = GameObject.FindObjectOfType<Score>();
+        }
+
+        if (musicController == null)
+        {
+            musicController = GameObject.FindObjectOfType<BackgroundMusicController>();
+        }
     }
 
     private void OnTriggerEnter(Collider other)
     {
+        // ตรวจสอบว่าเป็นสิ่งกีดขวางหรือไม่
         if (other.CompareTag("obstacle"))
         {
             Debug.Log("Player hit the obstacle!");
 
-            player.StopMove();
-            score.StopCount();
-            endScreen.SetActive(true);
-            ScoreCount.SetActive(false);
-            UIcontorl.SetActive(false);
+            // หยุดเพลงพื้นหลัง
+            if (musicController != null)
+            {
+                musicController.StopMusic();
+            }
+
+            // หยุดการเคลื่อนที่และการเพิ่มคะแนน
+            if (player != null)
+            {
+                player.StopMove();
+            }
+
+            if (score != null)
+            {
+                score.StopCount();
+            }
+
+            // แสดงจอเกมจบและซ่อน UI
+            if (endScreen != null)
+            {
+                endScreen.SetActive(true);
+            }
+
+            if (ScoreCount != null)
+            {
+                ScoreCount.SetActive(false);
+            }
+
+            if (UIControl != null)
+            {
+                UIControl.SetActive(false);
+            }
         }
-
-
-        /*if (collision.gameObject.name == "Player")
-        {
-            playerMovement.Die();
-        }*/
-    }
-
-
-    // Update is called once per frame
-    void Update()
-    {
-        
     }
 }
---
+++ Assets/Thee/Script/BackgroundMusicController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackgroundMusicController : MonoBehaviour
+{
+    private AudioSource audioSource;
+
+    private void Start()
+    {
+        // ดึง AudioSource จาก GameObject นี้
+        audioSource = GetComponent<AudioSource>();
+
+        // เล่นเพลงเมื่อซีนเริ่ม
+        if (audioSource != null)
+        {
+            audioSource.Play();
+        }
+    }
+
+    public void StopMusic()
+    {
+        AudioSource audioSource = GetComponent<AudioSource>();
+        if (audioSource != null && audioSource.isPlaying)
+        {
+            audioSource.Stop();
+        }
+    }
+}
